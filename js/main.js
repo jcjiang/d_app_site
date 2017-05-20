7339ba43f9a4f74ab4250b37798b86fc4604eb14
Jiachen <jcjiang42@gmail.com>
2017-05-20 01:52:28 -0400

    debugged scroll

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function showAside() {
+  document.getElementById("just_kidding").innerHTML = "Not too long, though.  You know what they say about guests and fish.";
+  document.getElementById("no_really").innerHTML = "But seriously, I love friends.  Sure wish I had some.  (I have a pretty bad sense of humor.)"
+}
+
+function makeWoodSmaller() {
+  document.getElementById("woodsmen_description").innerHTML = "In retrospect, that was a weird way to start off freshman year."
+}
+
+var onImg= "../img/woodsmen_before.jpg";
+var offImg= "../img/woodsmen_after.jpg";
